f3cd735360a5977b58a50cb86237dcb267cc78e3
ZafarsCreativeOrganizer <arkan.farid0@gmail.com>
2025-06-27 19:00:10 +0800

    login

--- js/login.js
+++ js/login.js
@@ -1,13 +1,11 @@
 // Import firebase.js
 // <script src="../firebase.js"></script> sudah diindex.html
 
-const auth = firebase.auth();
-
 document.getElementById('google-login').addEventListener('click', function() {
   const provider = new firebase.auth.GoogleAuthProvider();
   auth.signInWithPopup(provider)
     .then((result) => {
-      window.location.href = 'index.html';
+      window.location.href = 'dashboard.html';
     })
     .catch((error) => {
       alert('Login failed: ' + error.message);
@@ -16,6 +14,6 @@ document.getElementById('google-login').addEventListener('click', function() {
 
 auth.onAuthStateChanged(function(user) {
   if (user) {
-    window.location.href = 'index.html';
+    window.location.href = 'dashboard.html';
   }
 }); 
