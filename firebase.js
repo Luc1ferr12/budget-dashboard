174baa59dfbc015d82d1fcd8a4907707a893fb70
ZafarsCreativeOrganizer <arkan.farid0@gmail.com>
2025-06-27 18:44:45 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCtVKalCYsWX5eHfZyk005uzMElM_vreKo",
     authDomain: "data-pribadi-ac30b.firebaseapp.com",
     projectId: "data-pribadi-ac30b",
-    storageBucket: "data-pribadi-ac30b.firebasestorage.app",
+    storageBucket: "data-pribadi-ac30b.appspot.com",
     messagingSenderId: "428995046107",
     appId: "1:428995046107:web:0ed23242e750a3cb2cfafe",
     measurementId: "G-98E4KP24VD"
