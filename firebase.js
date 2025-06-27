7cbe957756191d849d56258ba9330af4d4f0437c
ZafarsCreativeOrganizer <arkan.farid0@gmail.com>
2025-06-27 19:08:31 +0800

    p

--- firebase.js
+++ firebase.js
@@ -4,10 +4,9 @@ const firebaseConfig = {
     apiKey: "AIzaSyCtVKalCYsWX5eHfZyk005uzMElM_vreKo",
     authDomain: "data-pribadi-ac30b.firebaseapp.com",
     projectId: "data-pribadi-ac30b",
-    storageBucket: "data-pribadi-ac30b.appspot.com",
+    storageBucket: "data-pribadi-ac30b.firebasestorage.app",
     messagingSenderId: "428995046107",
     appId: "1:428995046107:web:0ed23242e750a3cb2cfafe",
-    measurementId: "G-98E4KP24VD"
 };
 
 if (!firebase.apps.length) {
